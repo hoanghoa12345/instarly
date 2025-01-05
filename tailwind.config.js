f0beae68b453006c6252396ab728e065b623444c
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2025-01-05 18:06:44 +0700

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssMotion from "tailwindcss-motion";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -7,5 +9,5 @@ module.exports = {
       cursor: ["disabled"],
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwindcss-motion")],
+  plugins: [require("@tailwindcss/forms"), tailwindcssMotion],
 };
